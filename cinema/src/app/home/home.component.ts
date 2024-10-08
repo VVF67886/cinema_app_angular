1816a2754b62897a881d212ed5b2c04ddef3cf30
CapoFra1 <francy_cap@hotmail.it>
2024-10-08 17:59:16 +0200

    no message

--- cinema/src/app/home/home.component.ts
+++ cinema/src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { ServizioDatiService } from '../servizi/servizio-dati.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatToolbarModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.less'
+  styleUrl: './home.component.less',
 })
 export class HomeComponent {
-
+  constructor(private dati:ServizioDatiService){}
+  prova() {
+    this.dati.prova().subscribe(
+      dati =>
+      {console.log(dati)}
+    )
+  }
 }
--- cinema/src/app/servizi/servizio-dati.service.ts
+++ cinema/src/app/servizi/servizio-dati.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,15 @@ import { Injectable } from '@angular/core';
 export class ServizioDatiService {
   constructor(private http: HttpClient) {}
   url: string = 'http://www.ve.dipvvf.it/corsong/resources/test';
+  /* httpOptions = {
+    headers: new HttpHeaders({
+      'content-type': 'application/json',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+ */
   prova() {
-    return this.http.get(this.url)
+    return this.http.get(this.url);
   }
 }
