cb6f35784a1c460a431f75ace5f2bcae88b27139
CapoFra1 <francy_cap@hotmail.it>
2024-10-10 12:25:28 +0200

    Card Home

--- cinema/src/app/home/home.component.ts
+++ cinema/src/app/home/home.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { CinemaService } from '../cinema.service';
+import { NgFor} from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, RouterModule],
+  imports: [NgFor,MatCardModule, MatToolbarModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  cinemaService: CinemaService = inject(CinemaService);
+  public generi: Array<any> = [];
+  public film: Array<any> = [];
+
+  ngOnInit(): void {
+    this.cinemaService.getGeneri().subscribe((d) => {
+      this.generi = CinemaService.rowsToObjects(d);
+    });
+    this.cinemaService.getFilmPerGenere('Drammatico').subscribe((d) => {
+      this.film = CinemaService.rowsToObjects(d);
+    });
+  }
+}
